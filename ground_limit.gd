f02ccfea045bac0d98263a6c3a7f77ce0c8fd378
Pedro Mota <110642021+mota-pedro@users.noreply.github.com>
2026-07-01 20:09:56 -0300

    feat: jogador perde quando cai em um buraco

---
+++ ground_limit.gd
@@ -0,0 +1,9 @@
+# ground_limit.gd
+extends Area2D
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.take_damage(body.max_health)
