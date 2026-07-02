11dadb04f8ea699f028bda5251131e6068fd0a3c
Pedro Mota <110642021+mota-pedro@users.noreply.github.com>
2026-07-01 22:40:29 -0300

    feat: novo menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,10 +1,6 @@
 # main_menu.gd
 extends Control
 
-func _ready():
-	# Garante que o background começa animando
-	pass
-
 func _on_jogar_pressed():
 	get_tree().change_scene_to_file("res://primeira_fase.tscn")
 
