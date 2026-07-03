63d8fee25ce3ea328542f9e3b26050d8d4e6627a
Pedro Mota <110642021+mota-pedro@users.noreply.github.com>
2026-07-02 23:26:46 -0300

    feat: um monte de coisa

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,16 @@
-# main_menu.gd
 extends Control
 
+@onready var recorde_label: Label = $RecordeLabel
+
+func _ready():
+	var recorde = SaveGame.recorde_fase
+	if recorde <= 0:
+		recorde_label.text = "Nenhum recorde ainda"
+	else:
+		recorde_label.text = "Melhor fase: %d" % recorde
+
 func _on_jogar_pressed():
+	GameState.fase_atual = 1
 	get_tree().change_scene_to_file("res://primeira_fase.tscn")
 
 func _on_sair_pressed():
